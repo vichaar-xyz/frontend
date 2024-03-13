2e039b411f00424be2d2b961a2a18c2ef67dfe55
Priyanshu1710 <guptapriyanshu1710@gmail.com>
2024-03-14 03:14:34 +0530

    Updated header component.

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -22,15 +22,20 @@ function ProfileLink() {
 function Header() {
     return (
         <>
-            <header className="header">
-                <SearchBar />
-                <div className="header-icons">
-                    <img src="https://cdn.builder.io/api/v1/image/assets/TEMP/bf683c09f23cc1d208be62b0a715d2afc156e0219f92f717eb100e6172eacbc4?apiKey=7ba4ed5c97414425b9fc582a5867d5b9&" alt="Header icon" className="header-icon" />
-                    <img src="https://cdn.builder.io/api/v1/image/assets/TEMP/549f3bfc75c551cc8a6d0c18cb0e7f2eab99ab9662c3c1237649cce9205843fc?apiKey=7ba4ed5c97414425b9fc582a5867d5b9&" alt="Header icon" className="header-icon" />
-                    <img src="https://cdn.builder.io/api/v1/image/assets/TEMP/9c250e1b77ad2c84c9e8d284d240fee90654a8eff0f728108536a807c0de25ef?apiKey=7ba4ed5c97414425b9fc582a5867d5b9&" alt="Header icon" className="header-icon" />
-                </div>
-                <ProfileLink />
-            </header>
+            <div className="header_main_container">
+
+                <header className="header">
+                    <SearchBar />
+                    <div className="left_section">
+                        <div className="header-icons">
+                            <img src="https://cdn.builder.io/api/v1/image/assets/TEMP/bf683c09f23cc1d208be62b0a715d2afc156e0219f92f717eb100e6172eacbc4?apiKey=7ba4ed5c97414425b9fc582a5867d5b9&" alt="Header icon" className="header-icon" />
+                            <img src="https://cdn.builder.io/api/v1/image/assets/TEMP/549f3bfc75c551cc8a6d0c18cb0e7f2eab99ab9662c3c1237649cce9205843fc?apiKey=7ba4ed5c97414425b9fc582a5867d5b9&" alt="Header icon" className="header-icon" />
+                            <img src="https://cdn.builder.io/api/v1/image/assets/TEMP/9c250e1b77ad2c84c9e8d284d240fee90654a8eff0f728108536a807c0de25ef?apiKey=7ba4ed5c97414425b9fc582a5867d5b9&" alt="Header icon" className="header-icon" />
+                        </div>
+                        <ProfileLink />
+                    </div>
+                </header>
+            </div>
 
         </>
     );
